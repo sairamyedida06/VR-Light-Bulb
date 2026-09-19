dcf90b208563c6c4ac800a8d6c23a2fda733e257
sairamyedida06 <sairamyedidaoffl@gmail.com>
2026-09-19 23:29:04 +0530

    feat: animate power switch on toggle via Animator bool parameter

--- Assets/Scripts/PowerSwitch.cs
+++ Assets/Scripts/PowerSwitch.cs
@@ -5,10 +5,13 @@ public class PowerSwitch : MonoBehaviour
     // public get so other scripts can read power state, private set so only the switch can change it
     public bool IsPowerOn { get; private set; }
 
+    [SerializeField] private Animator animator;
+
     // Wired to the XR Simple Interactable's Select Entered event
     public void Toggle()
     {
         IsPowerOn = !IsPowerOn;
+        animator.SetBool("isOn", IsPowerOn);
         Debug.Log("Power is now: " + (IsPowerOn ? "ON" : "OFF"));
     }
 }
